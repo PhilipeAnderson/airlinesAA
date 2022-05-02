f259896956e09551f3d2daf9ae7c2a78ea068c8a
philipe_kraken <philipeacampos@hotmail.com>
2022-05-02 12:36:14 -0300

    deleted project like suggested dependabot from gitHub

--- src/components/ButtonSubmit/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css';
-
-export function ButtonSubmit({ name, link }){
-  return(
-    <button type="submit" className={styles.buttonLink} href={ link }>
-      { name }
-    </button>
-  )
-}
